c0ba033a4ab72691af08403278f9e2228673f73a
Marcus Wood <marcuswood230@gmail.com>
2025-02-27 11:54:57 -0500

    update to devvit 0.11.7, tailwind v4, react 19, vite 6, useWebView hook

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
-import { Devvit, useAsync, useState } from '@devvit/public-api';
+import { Devvit, useWebView } from '@devvit/public-api';
 import { DEVVIT_SETTINGS_KEYS } from './constants.js';
-import { sendMessageToWebview } from './utils/utils.js';
-import { WebviewToBlockMessage } from '../game/shared.js';
-import { WEBVIEW_ID } from './constants.js';
+import { BlocksToWebviewMessage, WebviewToBlockMessage } from '../game/shared.js';
 import { Preview } from './components/Preview.js';
 import { getPokemonByName } from './core/pokeapi.js';
 
@@ -48,60 +46,52 @@ Devvit.addCustomPostType({
   name: 'Experience Post',
   height: 'tall',
   render: (context) => {
-    const [launched, setLaunched] = useState(false);
+    const { mount } = useWebView<WebviewToBlockMessage, BlocksToWebviewMessage>({
+      onMessage: async (event, { postMessage }) => {
+        console.log('Received message', event);
+        const data = event as unknown as WebviewToBlockMessage;
+
+        switch (data.type) {
+          case 'INIT':
+            postMessage({
+              type: 'INIT_RESPONSE',
+              payload: {
+                postId: context.postId!,
+              },
+            });
+            break;
+          case 'GET_POKEMON_REQUEST':
+            context.ui.showToast({ text: `Received message: ${JSON.stringify(data)}` });
+            const pokemon = await getPokemonByName(data.payload.name);
+
+            postMessage({
+              type: 'GET_POKEMON_RESPONSE',
+              payload: {
+                name: pokemon.name,
+                number: pokemon.id,
+                // Note that we don't allow outside images on Reddit if
+                // wanted to get the sprite. Please reach out to support
+                // if you need this for your app!
+              },
+            });
+            break;
+
+          default:
+            console.error('Unknown message type', data satisfies never);
+            break;
+        }
+      },
+    });
 
     return (
       <vstack height="100%" width="100%" alignment="center middle">
-        {launched ? (
-          <webview
-            id={WEBVIEW_ID}
-            url="index.html"
-            width={'100%'}
-            height={'100%'}
-            onMessage={async (event) => {
-              console.log('Received message', event);
-              const data = event as unknown as WebviewToBlockMessage;
-
-              switch (data.type) {
-                case 'INIT':
-                  sendMessageToWebview(context, {
-                    type: 'INIT_RESPONSE',
-                    payload: {
-                      postId: context.postId!,
-                    },
-                  });
-                  break;
-                case 'GET_POKEMON_REQUEST':
-                  context.ui.showToast({ text: `Received message: ${JSON.stringify(data)}` });
-                  const pokemon = await getPokemonByName(data.payload.name);
-
-                  sendMessageToWebview(context, {
-                    type: 'GET_POKEMON_RESPONSE',
-                    payload: {
-                      name: pokemon.name,
-                      number: pokemon.id,
-                      // Note that we don't allow outside images on Reddit if
-                      // wanted to get the sprite. Please reach out to support
-                      // if you need this for your app!
-                    },
-                  });
-                  break;
-
-                default:
-                  console.error('Unknown message type', data satisfies never);
-                  break;
-              }
-            }}
-          />
-        ) : (
-          <button
-            onPress={() => {
-              setLaunched(true);
-            }}
-          >
-            Launch
-          </button>
-        )}
+        <button
+          onPress={() => {
+            mount();
+          }}
+        >
+          Launch
+        </button>
       </vstack>
     );
   },
